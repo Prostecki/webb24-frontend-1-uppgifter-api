d74fe5ee67191a30a81547fa07c8b31f99b66315
Mark Taratynov <mark.taratynov@gmail.com>
2024-10-06 14:43:53 +0200

    Task 4 done

--- task4.js
+++ task4.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 const renderButton = document.getElementById("render-button");
+const inputId = document.getElementById("input-title");
 const inputBody = document.getElementById("input-text");
 const form = document.getElementById("form");
 
@@ -7,7 +8,7 @@ let postsData = [];
 
 async function getData() {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
     if (!response.ok) {
       throw new Error(`Error, ${response.status}`);
     }
@@ -18,35 +19,75 @@ async function getData() {
   }
 }
 
-// async function putData() {}
+async function putData(userId, title, body) {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${userId}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          userId: userId,
+          id: userId,
+          title: title,
+          body: body,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    if (!response.ok) {
+      console.log(`HTTP Error!, ${response.status}`);
+    }
+    const putData = await response.json();
+    console.log(putData);
+  } catch (error) {
+    console.error("Something wrong!");
+  }
+}
 
 renderButton.addEventListener("click", (e) => {
-  let userId = parseInt(inputBody.value.trim());
   e.preventDefault();
-  getUserId(userId);
+
+  //Get values by inputs
+  let userId = parseInt(inputId.value.trim());
+  let userBody = inputBody.value.trim();
+
+  //Check the ID if it's a number
+  if (isNaN(userId) || !userBody) {
+    console.log("Invalid input.");
+  }
+  //Update data
+  putData(userId, `Updated body for Post ${userId}`, userBody);
+
+  //Clear inputs
   inputBody.value = "";
+  inputId.value = "";
+
+  //Show post
+  getUserId(userId);
 });
 
 function getUserId(userId) {
   container.innerHTML = "";
   const post = postsData.find((item) => item.id === userId);
   if (post) {
-    let userTitle = document.createElement("h2");
-    let userId = document.createElement("h4");
-    let userBody = document.createElement("p");
-    userTitle.textContent = `User Title: ${post.title}`;
-    userId.textContent = `User ID: ${post.id}`;
-    userBody.textContent = `User Text: ${post.body}`;
-    container.appendChild(userTitle);
-    container.appendChild(userId);
-    container.appendChild(userBody);
+    let postTitle = document.createElement("h2");
+    let postId = document.createElement("h4");
+    let postBody = document.createElement("p");
+    postTitle.textContent = `User Title: ${post.title}`;
+    postId.textContent = `User ID: ${post.id}`;
+    postBody.textContent = `User Text: ${post.body}`;
+    // container.appendChild(userTitle);
+    // container.appendChild(userId);
+    // container.appendChild(userBody);
   } else {
-    console.log(`Post with ID ${userId} not found.`);
-    getError(userId);
+    console.log(`Post with ID ${post} not found.`);
+    getError(post);
   }
 }
 
-function getError(userId) {
+function getError() {
   container.innerHTML = "";
   let userTitle = document.createElement("h2");
   userTitle.style.textAlign = "center";
